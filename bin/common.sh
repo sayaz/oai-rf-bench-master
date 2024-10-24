36e72f67ef11ca05200c1c894506f8ce42ad54e5
Syed Ayaz Mahmud <sayazmahmud@gmail.com>
2024-10-24 16:38:58 -0700

    Update common.sh

--- bin/common.sh
+++ bin/common.sh
@@ -1,6 +1,6 @@
 
 SRCDIR=/var/tmp
 CFGDIR=/local/repository/etc
-OAI_RAN_MIRROR="https://gitlab.flux.utah.edu/powder-mirror/openairinterface5g/-/tree/develop?ref_type=heads"
-OAI_CN5G_REPO="https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed/-/tree/develop?ref_type=heads"
+OAI_RAN_MIRROR="https://gitlab.flux.utah.edu/powder-mirror/openairinterface5g.git"
+OAI_CN5G_REPO="https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed"
 SRS_REPO="https://github.com/srsran/srsRAN"
